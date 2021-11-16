05b3abb5a94433a9201e29c7f743ddb5114b5562
RetiredWizard <github@retiredwizard.com>
2021-11-16 01:06:01 -0500

    GPIO updates for sound and I2C

--- sound.py
+++ sound.py
@@ -4,24 +4,18 @@ if sys.implementation.name.upper() == 'MICROPYTHON':
     import machine
 elif sys.implementation.name.upper() == 'CIRCUITPYTHON':
     from pwmio import PWMOut
+    from board import board_id
     foundPin = True
-    try:
+    if board_id == "arduino_nano_rp2040_connect":
         #A5 is GPIO D19 on Nano Connect
         from board import A5 as sndPin
-    except:
-        foundPin = False
-    if not foundPin:
-        foundPin = True
+    elif board_id == "raspberry_pi_pico":
+        #D12 is GP11 on the Raspberry PICO
+        from board import GP11 as sndPin
+    else:
         try:
-            #Use D12 on Feather
+            #Use D12 on Feathers
             from board import D12 as sndPin
-        except:
-            foundPin = False
-    if not foundPin:
-        foundPin = True
-        try:
-            #D12 is GP11 on the Raspberry PICO
-            from board import GP11 as sndPin
         except:
             foundPin = False
 
