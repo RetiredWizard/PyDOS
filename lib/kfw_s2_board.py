95ae0681c934da316c59bac97889e5dc36dd1536
RetiredWizard <github@retiredwizard.com>
2021-11-17 02:43:54 -0500

    Added all S2 pins

--- lib/kfw_s2_board.py
+++ lib/kfw_s2_board.py
@@ -32,6 +32,56 @@ D11 = board.D9
 D12 = board.D12
 D13 = board.D13
 
+A6 = board.A6
+A7 = board.A7
+A8 = board.A8
+A9 = board.A9
+A10 = board.A10
+AMB = board.AMB
+APA102_MOSI = board.APA102_MOSI
+APA102_SCK = board.APA102_SCK
+D0 = board.D0
+D1 = board.D1
+D4 = board.D4
+D15 = board.D15
+D16 = board.D16
+D17 = board.D17
+D18 = board.D18
+D19 = board.D19
+D20 = board.D20
+D21 = board.D21
+D23 = board.D23
+D24 = board.D24
+D25 = board.D25
+DAC1 = board.DAC1
+DAC2 = board.DAC2
+IO0 = board.IO0
+IO1 = board.IO1
+IO3 = board.IO3
+IO4 = board.IO4
+IO5 = board.IO5
+IO6 = board.IO6
+IO7 = board.IO7
+IO8 = board.IO8
+IO9 = board.IO9
+IO10 = board.IO10
+IO11 = board.IO11
+IO12 = board.IO12
+IO14 = board.IO14
+IO17 = board.IO17
+IO18 = board.IO18
+IO21 = board.IO21
+IO33 = board.IO33
+IO35 = board.IO35
+IO36 = board.IO36
+IO37 = board.IO37
+IO38 = board.IO38
+IO43 = board.IO43
+IO44 = board.IO44
+LDO2 = board.LDO2
+LED = board.LED
+board_id = board.board_id
+
 
 def SPI():
     global _SPI
