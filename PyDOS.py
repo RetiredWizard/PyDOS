50bebe4f5c1f82c43b50cc71e625a06824566b71
RetiredWizard <github@retiredwizard.com>
2021-11-17 23:23:50 -0500

    Version number update

--- PyDOS.py
+++ PyDOS.py
@@ -585,7 +585,7 @@ def PyDOS():
                         break
 
         elif cmd == "VER":
-            print("PyDOS [Version 0.91]")
+            print("PyDOS [Version 1.0]")
 
         elif cmd == "ECHO":
             if len(args) == 1:
