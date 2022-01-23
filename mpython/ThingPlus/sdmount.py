4ebbd43b210fd688327a7ba33f7c23d4fe6e3d6c
RetiredWizard <github@retiredwizard.com>
2022-01-23 17:42:02 -0500

    Delete sdmount.py

--- mpython/ThingPlus/sdmount.py
+++
@@ -1,9 +0,0 @@
-import sdcard
-drive = "/sd"
-
-if __name__ != "PyDOS":
-    passedIn = ""
-
-if passedIn != "":
-    drive = passedIn
-sd = sdcard.SDCard(spi=1, sck=14, mosi=15, miso=12, cs=9, drive=drive)
