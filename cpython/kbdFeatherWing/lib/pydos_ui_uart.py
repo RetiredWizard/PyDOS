e8427dd058a07fef7154986220ac1f2299bb2c45
RetiredWizard <github@retiredwizard.com>
2022-01-23 17:51:38 -0500

    Add files via upload

--- cpython/kbdFeatherWing/lib/pydos_ui_uart.py
+++ cpython/kbdFeatherWing/lib/pydos_ui_uart.py
@@ -8,15 +8,33 @@ if implementation.name.upper() == "MICROPYTHON":
 elif implementation.name.upper() == "CIRCUITPYTHON":
     from supervisor import runtime
     import digitalio
-    import board
-    #if board.board_id == "cytron_maker_pi_rp2040":
     import busio
+    import board
+    if board.board_id == "unexpectedmaker_feathers2":
+        try:
+            import kfw_s2_board as board
+        except:
+            pass
+    if board.board_id == "raspberry_pi_pico":
+        try:
+            import kfw_pico_board as board
+            foundBoard = True
+        except:
+            foundBoard = False
+
+        if not foundBoard:
+            try:
+                import cyt_mpp_board as board
+            except:
+                pass
+
 
 class PyDOS_UI:
 
     _I2C = None
     _I2C_power = None
     _SPI = None
+    SD_CS = None
 
     def __init__(self):
         pass
@@ -40,18 +58,30 @@ class PyDOS_UI:
                     PyDOS_UI._I2C_power.direction = digitalio.Direction.OUTPUT
                     PyDOS_UI._I2C_power.value = False
 
-                return board.I2C()
+                if 'I2C' in dir(board) and not PyDOS_UI._I2C:
+                    PyDOS_UI._I2C = board.I2C()
+                elif not PyDOS_UI._I2C:
+                    PyDOS_UI._I2C = busio.I2C(board.SCL, board.SDA)
+
+                return PyDOS_UI._I2C
+
+        def SD_CSdeinit():
+            PyDOS_UI.SD_CS.deinit()
+            PyDOS_UI.SD_CS = None
+ 
 
         def SPI():
-            if not PyDOS_UI._SPI:
-                try:
-                    PyDOS_UI._SPI = board.SPI()
-                except:
-                    if 'SCK' in dir(board):
-                        if 'MOSI' in dir(board):
-                            PyDOS_UI._SPI = busio.SPI(board.SCK, board.MOSI, board.MISO)
-                        elif 'COPI' in dir(board):
-                            PyDOS_UI._SPI = busio.SPI(board.SCK, board.COPI, board.CIPO)
+            if PyDOS_UI.SD_CS == None:
+                PyDOS_UI.SD_CS = digitalio.DigitalInOut(board.D5)
+
+            if 'SPI' in dir(board) and not PyDOS_UI._SPI:
+                PyDOS_UI._SPI = board.SPI()
+            elif not PyDOS_UI._SPI:
+                if 'SCK' in dir(board):
+                    if 'MOSI' in dir(board):
+                        PyDOS_UI._SPI = busio.SPI(board.SCK, board.MOSI, board.MISO)
+                    elif 'COPI' in dir(board):
+                        PyDOS_UI._SPI = busio.SPI(board.SCK, board.COPI, board.CIPO)
 
             return PyDOS_UI._SPI
 
