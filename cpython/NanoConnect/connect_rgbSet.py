fabf7ee434a5a68452cd843f914bd4a321a630e7
RetiredWizard <github@retiredwizard.com>
2022-01-23 22:38:18 -0500

    Turn off LEDs when no values entered
    Entering zero values does not shut off LEDs completely

--- cpython/NanoConnect/connect_rgbSet.py
+++ cpython/NanoConnect/connect_rgbSet.py
@@ -42,17 +42,19 @@ if len(ans.split(",")) == 3:
     g = 255 - max(0,min(255,int((ans.split(',')[1] if ans.split(",")[1].isdigit() else 0))))
     b = 255 - max(0,min(255,int((ans.split(',')[2] if ans.split(",")[2].isdigit() else 0))))
 else:
-    r = 0
-    g = 0
-    b = 0
+    r = -1
+    g = -1
+    b = -1
 
-esp.set_pin_mode(LEDR,OUT)
-esp.set_pin_mode(LEDG,OUT)
-esp.set_pin_mode(LEDB,OUT)
+if r != -1:
 
-esp.set_analog_write(LEDR,r/255)
-esp.set_analog_write(LEDG,g/255)
-esp.set_analog_write(LEDB,b/255)
+    esp.set_pin_mode(LEDR,OUT)
+    esp.set_pin_mode(LEDG,OUT)
+    esp.set_pin_mode(LEDB,OUT)
+
+    esp.set_analog_write(LEDR,r/255)
+    esp.set_analog_write(LEDG,g/255)
+    esp.set_analog_write(LEDB,b/255)
 
 esp32_cs.deinit()
 esp32_ready.deinit()
