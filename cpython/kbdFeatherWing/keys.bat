8468216f2e05f3f3da62411e715363a1a33b7e4f
RetiredWizard <github@retiredwizard.com>
2022-06-21 08:08:00 -0400

    Keyboard Featherwing key mapping description

---
+++ cpython/kbdFeatherWing/keys.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Alt-$ (Speaker) returns an equal sign (=)
+echo Hold a key for 2 seconds SHIFT or ALT
+echo Alt-Enter will toggle between SHIFT and ALT
+echo Alt-Space will enable SHIFT Lock or ALT Lock
+echo Shift when SHIFT Lock returns lower case
+echo The Microphone symbol returns a left bracket ([)
+echo 2 second Shift on Microphone = right bracket (])
+echo Shift Lock Microphone Symbol = right bracket (])
+echo 2 second Shift on dollar sign ($) = percent (%%)
+echo Shift Lock dollar sign ($) = percent (%%)
+echo joystick up/down scrolls through entered lines
+echo The F3 key retrieves the last entered line
+echo F2 and then pressing a key will search
+echo    the last entered line for that character
+pause
