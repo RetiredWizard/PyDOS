5d3cfd01b86806a23604159d95be5695f2c3129d
RetiredWizard <github@retiredwizard.com>
2022-10-05 22:40:00 -0400

    suppress confirmation for autoexec backup

--- 3.bat
+++ 3.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist /runvm.py goto micropython
-if exist autoexec.bat copy autoexec.bat autoexec.sav
+if exist autoexec.bat copy/y autoexec.bat autoexec.sav
 copy/y 3.aut autoexec.bat
 runvm "PyBasic/PyBasic adventure-fast.pgm"
 :micropython
